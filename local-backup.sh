d495b4cbd46d8aeb741e3db14b1d1c9296f4856e
Dmitri Popov <dmpop@linux.com>
2023-07-29 14:07:35 +0200

    Dialog prompts

--- local-backup.sh
+++ local-backup.sh
@@ -3,22 +3,35 @@
 # Author: Dmitri Popov, dmpop@cameracode.coffee
 # Source code: https://github.com/dmpop/ish-scripts
 
+# Specify ntfy_topic to enable ntfy.sh notifications
+ntfy_server="ntfy.sh"
+ntfy_topic=""
+
 if [ -z "$1" ]; then
-    echo "Specify a directory name"
+    dialog --erase-on-exit --backtitle "Error" --msgbox "Specify an identifier, e.g.:\n\n$0 CARD1" 9 32
     exit 1
 fi
 
 if [ ! -x "$(command -v rsync)" ]; then
     apk update
     apk upgrade
-    apk add rsync
+    apk add rsync dialog curl
 fi
 
 mkdir -p "$1"
+dialog --erase-on-exit --backtitle "Info" --msgbox "When prompted, choose the SOURCE folder." 7 30
 mount -t ios . "$1"
 mkdir -p backup
+dialog --erase-on-exit --backtitle "Info" --msgbox "When prompted, choose the DESTINATION folder." 7 30
 mount -t ios . backup
 
 rsync -av --info=progress2 --delete "$1" "backup/"
 umount "$1"
 umount backup
+
+if [ ! -z "$ntfy_topic" ]; then
+    curl \
+        -d "Backup completed. Have a nice day!" \
+        -H "Title: Message from iSH" \
+        "$ntfy_server/$ntfy_topic" >/dev/null 2>&1
+fi
--- remote-backup.sh
+++ remote-backup.sh
@@ -9,18 +9,29 @@ remote_user="user"
 remote_password="secret"
 remote_server="hello.xyz"
 remote_dir="/path/to/remote/dir"
+# Specify ntfy_topic to enable ntfy.sh notifications
+ntfy_server="ntfy.sh"
+ntfy_topic=""
 # ------------------------------
 
 if [ ! -x "$(command -v rsync)" ] || [ ! -x "$(command -v sshpass)" ]; then
     apk update
     apk upgrade
-    apk add rsync sshpass
+    apk add rsync sshpass dialog
 fi
 
 mkdir -p "$local_dir"
+dialog --erase-on-exit --backtitle "Info" --msgbox "When prompted, choose the LOCAL folder." 7 30
 mount -t ios . "$local_dir"
 
 sshpass -p "$remote_password" rsync -avhz --exclude=".*" --info=progress2 --delete -P -e "ssh -p 22" \
     "$local_dir/" "$remote_user"@"$remote_server":"$remote_dir"
 
 umount "$local_dir"
+
+if [ ! -z "$ntfy_topic" ]; then
+    curl \
+        -d "Backup completed. Have a nice day!" \
+        -H "Title: Message from iSH" \
+        "$ntfy_server/$ntfy_topic" >/dev/null 2>&1
+fi
